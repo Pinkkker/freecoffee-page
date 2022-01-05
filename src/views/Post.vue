e1118c275e0fdf687b50e9581f79c4dc1db8077d
dongdongbytepower <87803219+dongdongbytepower@users.noreply.github.com>
2022-01-05 14:27:31 +0800

    Update Post.vue

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -2,72 +2,205 @@
   <el-container>
     <el-main>
       <div class="all">
-        <div class="temp">
-          biapto
-          <el-avatar :size="32" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-          <el-divider></el-divider>
-          asdasd
-        </div>
-        <div class="writer">
-          <div class="writer-body">
-            <el-avatar :size="65" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-            <el-button type="success">关注作者</el-button>
+        <div class="" v-for="v in postcontent" :key="v.id">
+        <el-row>
+          <el-col :span="24"><div class="postcontent">
+            <div class="temp">
+              {{v.contents}}
+            </div>
+          </div></el-col>
+        </el-row>
+        <el-col :span="5"><div class="postdata">
+          发布于{{v.time}}
+        </div></el-col>
+      </div>
+        <el-row>
+        <div class="cocommen">
+          <el-col :span="10"><div class="createcommen">
+            <el-input
+                type="textarea"
+                autosize
+                placeholder="撰写评论"
+                v-model="createComment">
+            </el-input>
+            <div class="commit">
+              <el-button size="small" type="primary" @click="posthuifu">提交</el-button>
+            </div>
           </div>
+          </el-col>
         </div>
+        </el-row>
+          <el-row>
+            <el-col :span="20"><div class="comment" v-for="v in dataList" :key="v.id">
+              <div class="auther">{{v.userName}}:</div>
+              <div class="content">内容{{v.contents}}</div>
+              <div class="commentdata">{{v.time}}</div>
+              <div class="response"><div class="responsecomment" @click="isresponsecomment">回复</div>
+                <h1 class="huifu" v-if="ishuifu">
+                  <div class="createcommen">
+                    <el-input
+                        type="textarea"
+                        autosize
+                        placeholder="撰写评论"
+                        v-model="responsecomment">
+                    </el-input>
+                    <div class="commit">
+                      <el-button size="small" type="primary" @click="commenthuifu">提交</el-button>
+                    </div>
+                  </div>
+                </h1>
+              </div>
+            </div>
+            </el-col>
+          </el-row>
       </div>
     </el-main>
   </el-container>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
-  name: "Post"
+  name: "Post",
+  data() {
+    return {
+      ishuifu: false,
+      dataList: [],
+      postcontent: '',
+      postId: "2",
+      createComment: '',
+      responsecomment: '',
+      postdata: "",
+      pageNum: '1',
+      pageSize: '4',
+    }
+  },
+  methods: {
+    posthuifu: function () {
+    },
+    isresponsecomment: function () {
+      this.ishuifu = !this.ishuifu;
+    },
+    commenthuifu: function () {
+    },
+  },
+  mounted() {
+    axios.get("/api/v1/comments/", {
+          params: {
+            pageNum: this.pageNum,
+            pageSize: this.pageSize,
+            postId: this.postId,
+          },
+        }).then((response) => {
+          this.dataList = response.data.data;
+          this.totalNum = response.data.totalNum;
+          this.toalPage = response.data.totalPage;
+          console.log(this.dataList.length);
+        });
+    axios.get("/api/v1/posts/" + "shopId", {
+      params: {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize,
+        postId: this.postId,
+      },
+    }).then((response) => {
+      this.postcontent = response.data.data;
+      this.totalNum = response.data.totalNum;
+      this.toalPage = response.data.totalPage;
+      console.log(this.postcontent)
+    });
+  },
 }
 </script>
 
 <style scoped>
+.el-container {
+  display: flex;
+}
 .el-main {
-  background-color: #dee2e6;
+  background: #b3c0d1;
   color: #333;
-  text-align: center;
-  line-height: 160px;
-  height: 950px;
-  display: flex;
+  line-height: 200px;
   justify-content: center;
-}
-
-.temp {
-  width: 800px;
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-}
-
-.writer {
   display: flex;
-  height: 190px;
-  width: 300px;
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
 }
-
-.writer-body {
-  padding: 20px;
-  width: 260px;
-  display: flex;
-  flex-direction: column;
-}
-
-.el-button--success{
-  margin-top: 45px;
-}
-
 .all {
   display: flex;
-  width: 1130px;
-  flex-direction: row;
-  justify-content: space-between;
+  flex-direction: column;
+  width: 900px;
+  height: 600px;
+}
+.temp {
+  width: 800px;
+  height: 150px;
+  margin-bottom: -80px;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+}
+.postdata {
+  display: flex;
+  justify-content: center;
+  margin-right: 140px;
+  background: #b3c0d1;
+  height: 0;
+  font-size: 15px;
+}
+.postcontent {
+  display: flex;
+}
+.createcommen {
+  margin-top: 120px;
+  margin-left: 60px;
+  width: 500px;
+  height: 20px;
+  display: flex;
+}
+.cocommen {
+display: flex;
+}
+.commit {
+  margin-left: 30px;
+  display: flex;
+  margin-top: 12px;
+  align-items: center;
+}
+.comment{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+}
+.content {
+  background-color: white;
+  display: flex;
+  line-height: 60px;
+  width: 800px;
+  margin-top: 30px;
+  margin-bottom: -80px;
+}
+.commentdata {
+  display: flex;
+  width: 50px;
+  height: 50px;
+  margin-bottom: -20px;
+}
+.response {
+  display: grid;
+  width: 50px;
+  height: 100px;
+}
+.huifu {
+  margin-top: -200px;
+  height: 50px;
+}
+.responsecomment {
+}
+.auther {
+  display: flex;
+  height: 90px;
+  width: 50px;
+  margin-top: -40px;
 }
 </style>
